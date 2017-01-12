53baa49537c61ae088e692f51eaf0927ea1e743e
Tommy Thompson <tomthompson828@gmail.com>
2017-01-12 16:48:26 -0500

    junking in favor of materialize

--- imports/ui/pages/LogWorkout2.jsx
+++ imports/ui/pages/LogWorkout2.jsx
@@ -8,24 +8,55 @@ import TextField from 'material-ui/TextField';
 import {List, ListItem} from 'material-ui/List';
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import Divider from 'material-ui/Divider';
+import DatePicker from 'material-ui/DatePicker';
 
 // Page
 const LogWorkout2 = () => (
   <MuiThemeProvider>
-    <Card>
-      <CardHeader
-        title="Without Avatar"
-        subtitle="Subtitle"
-        actAsExpander={true}
-        showExpandableButton={true}
-      />
-      <CardText expandable={true}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-        Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-        Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-      </CardText>
-    </Card>
+    <div>
+
+      {/* Title with Date Picker */}
+      <Card>
+        <CardHeader>
+           <h4>[Workout Name]</h4>
+           <h5>[Workout Day]</h5>
+           <DatePicker hintText="Date of Workout" firstDayOfWeek={0} defaultDate={new Date()} />
+         </CardHeader>
+      </Card>
+
+      <Divider />
+      <br />
+
+      {/* List of Excerises */}
+      <Card>
+        <CardHeader
+          title="[Excerise Name]"
+          actAsExpander={true}
+          showExpandableButton={true}
+        />
+        <CardText expandable={true}>
+          <TextField
+            hintText="100"
+            floatingLabelText="Set 1 (x15)"
+            floatingLabelFixed={true}
+            underlineShow={false}
+          />
+        </CardText>
+      </Card>
+
+      <br />
+
+      <Card>
+        <CardHeader
+          title="[Excerise Name]"
+          actAsExpander={true}
+          showExpandableButton={true}
+        />
+      </Card>
+
+
+
+    </div>
   </MuiThemeProvider>
 
 );
