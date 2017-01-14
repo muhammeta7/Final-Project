43c606aec90eb71a457e1bc005b498f892c00d8e
Tommy Thompson <tomthompson828@gmail.com>
2017-01-13 20:18:34 -0500

    finally found a working grid system

--- imports/ui/pages/LogWorkout2.jsx
+++ imports/ui/pages/LogWorkout2.jsx
@@ -2,7 +2,10 @@
 import React from 'react';
 import { Component } from 'react';
 
-// Import Material-ui
+// Import React Grid System
+import { Container, Row, Col, Visible, Hidden } from 'react-grid-system';
+
+// Import Material-ui 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TextField from 'material-ui/TextField';
 import {List, ListItem} from 'material-ui/List';
@@ -10,53 +13,87 @@ import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import Divider from 'material-ui/Divider';
 import DatePicker from 'material-ui/DatePicker';
 
+
 // Page
 const LogWorkout2 = () => (
   <MuiThemeProvider>
-    <div>
+    <Container>
 
       {/* Title with Date Picker */}
-      <Card>
-        <CardHeader>
-           <h4>[Workout Name]</h4>
-           <h5>[Workout Day]</h5>
-           <DatePicker hintText="Date of Workout" firstDayOfWeek={0} defaultDate={new Date()} />
-         </CardHeader>
-      </Card>
+      <Row>
+        <Card>
+          <CardHeader>
+             <h3>[Workout Name]</h3>
+             <h4>[Workout Day]</h4>
+             <DatePicker hintText="Date of Workout" firstDayOfWeek={0} defaultDate={new Date()} underlineShow={false} />
+           </CardHeader>
+        </Card>
+      </Row>
 
-      <Divider />
       <br />
+
 
       {/* List of Excerises */}
-      <Card>
-        <CardHeader
-          title="[Excerise Name]"
-          actAsExpander={true}
-          showExpandableButton={true}
-        />
-        <CardText expandable={true}>
-          <TextField
-            hintText="100"
-            floatingLabelText="Set 1 (x15)"
-            floatingLabelFixed={true}
-            underlineShow={false}
+      <Row>
+        <Card>
+          <CardHeader
+            title="[Excerise Name]"
+            actAsExpander={true}
+            showExpandableButton={true}
           />
-        </CardText>
-      </Card>
+          <CardText expandable={true}>
 
-      <br />
+            <Container>
+              <Row>
 
-      <Card>
-        <CardHeader
-          title="[Excerise Name]"
-          actAsExpander={true}
-          showExpandableButton={true}
-        />
-      </Card>
+                <Col sm={4} xs={6}>
+                  <TextField
+                    hintText="100"
+                    floatingLabelText="Set 1 (x25)"
+                    floatingLabelFixed={true}
+                    underlineShow={false}
+                  />
+                </Col>
+
+                <Col sm={4} xs={6}>
+                  <TextField
+                    hintText="150"
+                    floatingLabelText="Set 2 (x15)"
+                    floatingLabelFixed={true}
+                    underlineShow={false}
+                  />
+                </Col>
+
+                <Col sm={4} xs={6}>
+                  <TextField
+                    hintText="165"
+                    floatingLabelText="Set 3 (x10)"
+                    floatingLabelFixed={true}
+                    underlineShow={false}
+                  />
+                </Col>
+
+                <Col sm={4} xs={6}>
+                  <TextField
+                    hintText="175"
+                    floatingLabelText="Set 4 (x8)"
+                    floatingLabelFixed={true}
+                    underlineShow={false}
+                  />
+                </Col>
+
+              </Row>
+
+            </Container>
+
+          </CardText>
+        </Card>
+
+      </Row>
 
 
+    </Container>
 
-    </div>
   </MuiThemeProvider>
 
 );
