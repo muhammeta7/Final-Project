e783eacf0be45ff3d688a262be2b9b008827b45d
Tommy Thompson <tomthompson828@gmail.com>
2017-01-19 19:53:08 -0500

    removed MuiThemeProvider from log and create workout pages

--- imports/ui/pages/LogWorkout.jsx
+++ imports/ui/pages/LogWorkout.jsx
@@ -56,64 +56,59 @@ class LogWorkout extends Component {
 
   render() {
     return (
+      <Container>
 
-      <MuiThemeProvider>
-        <Container>
-
-          {/* Title with Date Picker */}
-          <Row>
-            <LogWorkoutDate
-              _routineName={this.state.routineName}
-              _workoutName={this.state.workoutName}
-            />
-          </Row>
+        {/* Title with Date Picker */}
+        <Row>
+          <LogWorkoutDate
+            _routineName={this.state.routineName}
+            _workoutName={this.state.workoutName}
+          />
+        </Row>
 
 
-          {/* List of Excerises */}
-          <Row>
+        {/* List of Excerises */}
+        <Row>
 
-            {/* ++++++++++ ITERATE OVER EXERCISES ++++++++++ */}
-            {this.state.exercises.map(function(search, i) {
+          {/* ++++++++++ ITERATE OVER EXERCISES ++++++++++ */}
+          {this.state.exercises.map(function(search, i) {
 
-              return (
-                <div key={"routine-" + this.state.routineName + "-workout-" + this.state.workoutName + "-exercise-" + i} >
-                  <br/>
-                  <LogExercise
-                    _routineId={this.state.routineName}
-                    _workoutId={this.state.workoutName}
+            return (
+              <div key={"routine-" + this.state.routineName + "-workout-" + this.state.workoutName + "-exercise-" + i} >
+                <br/>
+                <LogExercise
+                  _routineId={this.state.routineName}
+                  _workoutId={this.state.workoutName}
 
-                    _exerciseId={i}
-                    _exerciseName={search.exerciseName}
-                    _exerciseUnit={search.exerciseUnit}
+                  _exerciseId={i}
+                  _exerciseName={search.exerciseName}
+                  _exerciseUnit={search.exerciseUnit}
 
-                    _repArray={search.reps}
-                  />
-                </div>
-              );
+                  _repArray={search.reps}
+                />
+              </div>
+            );
 
-            }.bind(this))}
-            {/* ++++++++++++++++++++++++++++++++++++++++++++ */}
+          }.bind(this))}
+          {/* ++++++++++++++++++++++++++++++++++++++++++++ */}
 
-          </Row>
+        </Row>
 
-          <br />
-          {/* Submit or Cancel Form Submisson */}
-          {/* This will need a way to collect all the data from the forms above and then hit an api on the backend */}
-          {/* Maybe add a confirmation modal too... Create this workout? You will not be able to edit after this */}
-          <Row>
-            <center>
-              <Row>
-                <RaisedButton label="Submit" primary={true} onClick={this._uploadWorkout.bind(this)} />
-                <span> </span>
-                <RaisedButton label="Cancel" onClick={this._cancelWorkout.bind(this)} />
-              </Row>
-            </center>
-          </Row>
+        <br />
+        {/* Submit or Cancel Form Submisson */}
+        {/* This will need a way to collect all the data from the forms above and then hit an api on the backend */}
+        {/* Maybe add a confirmation modal too... Create this workout? You will not be able to edit after this */}
+        <Row>
+          <center>
+            <Row>
+              <RaisedButton label="Submit" primary={true} onClick={this._uploadWorkout.bind(this)} />
+              <span> </span>
+              <RaisedButton label="Cancel" onClick={this._cancelWorkout.bind(this)} />
+            </Row>
+          </center>
+        </Row>
 
-
-        </Container>
-      </MuiThemeProvider>
-
+      </Container>
     );
   }
 
