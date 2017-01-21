92c00ec7634f9a5599a17147c9c84ace84151b71
Tyler Geerdts <tygee713@gmail.com>
2017-01-21 17:36:49 -0500

    changed chart

--- imports/ui/components/ProgressionGraph.jsx
+++ imports/ui/components/ProgressionGraph.jsx
@@ -9,7 +9,36 @@ import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 
 // Create Component
 class ProgressGraph extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: {
+        title: 'Progress over time',
+        hAxis: {title: 'Weight', minValue: 0, maxValue: 315},
+        vAxis: {title: 'Date', minValue: 0, maxValue: 20},
+        legend: 'none'
+      },
+      rows: [
+        [1, 135, 155],
+        [2, 155, 175],
+        [3, 160, 180]
+      ],
+      columns: [
+        {
+          'type': 'number',
+          'label': 'Date'
+        },
+        {
+          'type': 'number',
+          'label': 'Bench Press'
+        },
+        {
+          'type': 'number',
+          'label': 'Squat'
+        }
+      ]
+    }
+  }
   render(){
     return(
       <Card>
@@ -17,9 +46,10 @@ class ProgressGraph extends Component {
           <h2>Your Progress</h2>
           <div className={"my-pretty-chart-container"}>
             <Chart
-              chartType="ScatterChart" 
-              data={[['Age', 'Weight'], [8, 12], [4, 5.5]]}
-              options={{}}
+              chartType="ScatterChart"
+              rows={this.state.rows}
+              columns={this.state.columns}
+              options={this.state.options}
               graph_id="ScatterChart"
               width="100%"
               height="400px"
@@ -28,9 +58,18 @@ class ProgressGraph extends Component {
           </div>
         </CardHeader>
       </Card>
-    );
+    )
   }
-};
+}
 
+
+// old chart:
+// chartType="ScatterChart" 
+// data={[['Age', 'Weight'], [8, 12], [4, 5.5]]}
+// options={{}}
+// graph_id="ScatterChart"
+// width="100%"
+// height="400px"
+// legend_toggle
 
 export default ProgressGraph;
