31cf32ef9384c8bdc2c171862ce9c5ee060c452c
Krishna <Yellayi@Krishnas-MacBook-Pro.local>
2017-01-25 19:18:55 -0500

    routines are showing

--- imports/ui/pages/UserProfile.jsx
+++ imports/ui/pages/UserProfile.jsx
@@ -20,10 +20,53 @@ import Divider from 'material-ui/Divider';
 import DatePicker from 'material-ui/DatePicker';
 import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
 
 class UserProfile extends Component{
+
+
+
+
 	constructor(props){
     	super(props);
+    	this.state = {routine_ids : [], routines: [], value: ""};
+	}
+
+
+	routines(){
+		return Routine.find({user_id: Meteor.userId()}).fetch()
+	}
+
+	handleChange = (event,index,value) => this.setState({value: value});
+
+	componentWillMount(){
+
+		Meteor.call("getRoutines", (err,res) => {
+			if(err) throw err;
+
+			this.setState({routine_ids: res})
+			console.log(res)
+
+			Meteor.call("getRoutineNames", res, (err,res2) => {
+
+				this.setState({routines: res2})
+				console.log(res2)
+			})
+		})
+		
+
+	}
+	renderRoutines(){
+		let routines = this.state.routines;
+		let routine_id = this.state.routine_ids;
+
+		return routines.map((routine,routine_id) => {
+
+			return (
+				<MenuItem key={routine_id} value={routine_id} primaryText={routine}/>
+			);
+		});
 	}
 	 
 
@@ -42,6 +85,13 @@ class UserProfile extends Component{
 				      Lets Take a look at your progress over the past.
 				    </CardText>
 				    <Row>
+				    	<center>
+				    		<DropDownMenu value={this.state.value} onChange={this.handleChange}>
+				    			{this.renderRoutines()}
+				    		</DropDownMenu>
+				    	</center>
+				    </Row>
+				    <Row>
 		                <center>
 		                  <Row>
 		                  	<RaisedButton style={buttonStyle} label="Dashboard" secondary={true} href = "dashboard"/>
@@ -50,6 +100,12 @@ class UserProfile extends Component{
 		                </center>
 		            </Row>
 				    
+				</Card>
+			</div>
+			<div>
+				<Card>
+					<CardTitle title="Your current routine"/>
+
 				</Card>
 			</div>
 		</Container>
