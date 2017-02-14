d6e11a514163090ca94b26ce47bd74af2fd67af9
Tommy Thompson <tomthompson828@gmail.com>
2017-02-13 20:41:45 -0500

    nvm. just unresrtict all outside urls

--- mobile-config.js
+++ mobile-config.js
@@ -1,5 +1,6 @@
 // Allow the Andriod (or iOS) app to use the Material UI icons on the Dashboard Page
-App.accessRule('https://fonts.googleapis.com/*');
+// App.accessRule('https://fonts.googleapis.com/*');
+App.accessRule("*");
 
 // Refer to this for more info...
 // http://stackoverflow.com/questions/29934218/external-images-not-being-displayed-in-android-app-meteor-cordova
