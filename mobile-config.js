c14fc766cc27a07f7aa7549e918ce02343c6bf14
Tommy Thompson <tomthompson828@gmail.com>
2017-02-13 20:36:03 -0500

    adding google fonts api to mobile

--- mobile-config.js
+++ mobile-config.js
@@ -1,5 +1,5 @@
 // Allow the Andriod (or iOS) app to use the Material UI icons on the Dashboard Page
-App.accessRule('http://maps.googleapis.com/*');
+App.accessRule('https://fonts.googleapis.com/*');
 
 // Refer to this for more info...
 // http://stackoverflow.com/questions/29934218/external-images-not-being-displayed-in-android-app-meteor-cordova
